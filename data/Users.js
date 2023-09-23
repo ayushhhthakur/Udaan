5fb2edc73ca7a4bac670e7c1f13ea493e64b4b0c
Ayush Thakur <ayushthakur1412@gmail.com>
2023-09-23 17:57:01 +0530

    Added Users Data

---
+++ data/Users.js
@@ -0,0 +1,15 @@
+export const USERS = [
+    {
+        user: "ayushthakur",
+        image: "https://instagram.fagr1-2.fna.fbcdn.net/v/t51.2885-19/376271559_650738170160562_3512151195955521682_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fagr1-2.fna.fbcdn.net&_nc_cat=106&_nc_ohc=r98GXW7OWB4AX8pN4kt&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfA-9Pl6zq8-PHL8bgupMohTzvGajm8IZMkz-ZkrMKgy0A&oe=6512EC70&_nc_sid=8b3546",
+    },
+    {
+        user: "manavjamwal",
+        image: "https://instagram.fagr1-4.fna.fbcdn.net/v/t51.2885-19/375528822_785557640035719_5990623021877511518_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fagr1-4.fna.fbcdn.net&_nc_cat=103&_nc_ohc=Zo13gUdBcxEAX-PS4uc&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAAl-NBD-lxHpdhYViEOz8EkKYWh4XBPkZG7BaPA4joBA&oe=6512E0F3&_nc_sid=8b3546",
+    },
+    {
+        user: "farheenshaad",
+        image: "https://instagram.fagr1-4.fna.fbcdn.net/v/t51.2885-19/358787080_935335090858694_7031224204854953543_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fagr1-4.fna.fbcdn.net&_nc_cat=102&_nc_ohc=QDa4kGYPpCcAX-Sz-sG&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAMMQ97snT8pTGZ7-N8LZzO8NgV0QImJzFQULHNi55Cog&oe=65145CEA&_nc_sid=8b3546",
+    },
+
+]
