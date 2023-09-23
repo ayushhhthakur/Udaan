643e25a599d704ac9e04658eb208c2d1dac7bb64
Ayush Thakur <ayushthakur1412@gmail.com>
2023-09-23 17:57:21 +0530

    Added Stories Component

---
+++ components/home/Stories.js
@@ -0,0 +1,43 @@
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
+import React from 'react'
+import { USERS } from '../../data/Users'
+
+
+const Stories = () => {
+  return (
+    <View style={{ marginTop: 13 }}> 
+    {/* <View style={{ marginBottom: 13 }}>  */}
+        <ScrollView 
+        horizontal 
+        showHorizontalScrollIndicator={false}>
+            {USERS.map((story, index) => (
+                <View key={index} style={{ alignItems: 'center' }}>
+                <Image source={{uri: story.image}}
+                style={styles.story} />
+                <Text style={styles.userNames}>{
+                    story.user.length > 15 ? story.user.slice(0, 10).toLowerCase()
+                    : story.user.toLowerCase()}
+                </Text>
+                </View>
+            ))}
+        </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create ({
+    story: {
+        width: 80,
+        height: 80,
+        borderRadius: 50,
+        marginLeft: 9,
+        borderWidth: 3,
+        borderColor: '#ff8701'
+    },
+    userNames: {
+        color: 'white',
+        fontSize: 12,
+    }
+})
+
+export default Stories
