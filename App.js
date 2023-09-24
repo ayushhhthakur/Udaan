f0f643d01e2f894d3ff91415e5bf14bf98fc9651
Ayush Thakur <ayushthakur1412@gmail.com>
2023-09-24 21:36:51 +0530

    Updated

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
-import Messeges from "./screens/Messeges";
-import Profile from "./screens/Profile";
+// import Messeges from "./screens/Messeges";
+// import Profile from "./screens/Profile";
 
 export default function App() {
   return (
     <HomeScreen />
-    <Messeges />
-    <Profile />
+    // <Messeges />
+    // <Profile />
   );
 }
